188b4364b5e3977644ac0c3234efede9575525db
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-12-05 22:29:32 +0500

    Update run script (Add voice generators start)

--- back/run.py
+++ back/run.py
@@ -1,21 +1,60 @@
 import atexit
 import json
 import multiprocessing
+from handlers.voice_generation.voice_generator import VoiceGenerator
 from listeners.listener import start_listeners
 from app import main as start_app
 from decouple import config
 
-def exit_handler():
-    listeners.terminate()
-    app.terminate()
+def start_voice_generator(redis_storage, table, queue_name, tts_model_url, iam_token, folder_id, return_voice_channel):
+    voice_generator = VoiceGenerator(
+        redis_storage=redis_storage,
+        table=table,
+        queue_name=queue_name,
+        tts_model_url=tts_model_url,
+        iam_token=iam_token,
+        folder_id=folder_id,
+        return_voice_channel=return_voice_channel
+    )
+    voice_generator.start_listening()
 
+
+
+def exit_handler(process):
+    if process.is_alive():
+        process.terminate()
+        process.join()
+        
+
+    
 if __name__ == '__main__':
-    atexit.register(exit_handler)
     with open('utils/config.json', 'r') as config_file:
-        listeners = multiprocessing.Process(target=start_listeners, args=(config('REDIS_STORAGE'), json.load(config_file),))
+        json_file = json.load(config_file)
+        
+        listeners = multiprocessing.Process(target=start_listeners, args=(config('REDIS_STORAGE'), json_file,))
+        atexit.register(exit_handler, listeners)
+        
+        redis_storage = config('REDIS_STORAGE')
+        table = int(json_file['redis']['generating_queue_table'])
+        queue_name = json_file['redis']['generating_queue_table_keys']['voice']
+        tts_model_url = json_file['yc']['tts_url']
+        iam_token = config('YC_IAM_TOKEN')
+        folder_id = config('YC_FOLDER_ID')
+        return_voice_channel = json_file['redis']['channels']['voice_generated']
+        
+        voice_generator_process = multiprocessing.Process(
+            target=start_voice_generator,
+            args=(redis_storage, table, queue_name, tts_model_url, iam_token, folder_id, return_voice_channel)
+        )
+        atexit.register(exit_handler, voice_generator_process)
+        
+                                         
     app = multiprocessing.Process(target=start_app)
+    atexit.register(exit_handler, app)
     
     listeners.start()
     app.start()
+    voice_generator_process.start()
     listeners.join()
     app.join()
+    voice_generator_process.join()
