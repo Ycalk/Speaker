2eb817fe2c5ad066c831570e9c9d7d5065cd60d3
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-12-10 21:55:08 +0500

    Add temp data remove

--- back/listeners/impl/video_generated.py
+++ back/listeners/impl/video_generated.py
@@ -27,7 +27,7 @@ class VideoGeneratedListener (Listener):
             path_in_bucket = f'video/{data["celebrity_code"].replace("_", "/")}/{data["user_name"]}.mp4'
             self.s3.upload_file(data['video'], self.generated_bucket, path_in_bucket)
             self.logger.info("Video was upload: %s", data)
-            
+            os.remove(data['video'])
             data['video'] = f'{os.getenv("DATA_STORAGE")}/{self.generated_bucket}/{path_in_bucket}'
             await self._redis.publish(self.generated_channel, json.dumps(data))
         
