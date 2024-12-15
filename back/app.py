6a0ff2a709b82d8ecbb380a6e9d24410c2891ca8
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-12-15 23:18:47 +0500

    Add name validation antispam system

--- back/app.py
+++ back/app.py
@@ -4,9 +4,13 @@ import json
 import re
 import os
 import aioredis
+from time import time
 
 app = Quart(__name__)
 NAME_API_URL = os.getenv('NAME_API_URL')
+VALIDATE_NAME_TIME_WINDOW = os.getenv('VALIDATE_NAME_TIME_WINDOW')
+VALIDATE_NAME_SPAM_THRESHOLD = os.getenv('VALIDATE_NAME_SPAM_THRESHOLD')
+
 
 with open('utils/config.json', 'r', encoding='utf-8') as config_file:
     config_data = json.load(config_file)
@@ -38,9 +42,31 @@ async def get_celebrities():
 async def get_queue_length():
     return jsonify({"queue_length": await redis.llen(to_generate_key)})
 
+
+validate_name_request_cache = {}
+
 @app.route('/validate', methods=['POST'])
 async def validate():
-    name = (await request.get_json()).get('name')
+    global validate_name_request_cache
+    
+    json_data = await request.get_json()
+    name = json_data.get('name')
+    user_id = json_data.get('user_id')
+    
+    if not user_id:
+        return jsonify({"valid": False, "gender": "NEUTRAL"}), 400
+    
+    current_time = time()
+    
+    validate_name_request_cache = {k: [t for t in v if current_time - t < VALIDATE_NAME_TIME_WINDOW] 
+                              for k, v in validate_name_request_cache.items()}
+    
+    if user_id in validate_name_request_cache and len(validate_name_request_cache[user_id]) >= VALIDATE_NAME_SPAM_THRESHOLD:
+        return jsonify({"valid": False, "gender": "NEUTRAL"}), 400
+    
+    if user_id not in validate_name_request_cache:
+        validate_name_request_cache[user_id] = []
+    
     is_name, gender = await validate_name(name)
     return jsonify({"valid": is_name, "gender": gender}), 200
     
