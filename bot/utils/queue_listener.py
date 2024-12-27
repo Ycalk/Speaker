187b35a50f729343d8308ee246f537415f9d839e
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-12-27 21:16:39 +0500

    Fix queue listener

--- bot/utils/queue_listener.py
+++ bot/utils/queue_listener.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot
 import aioredis
 import json
+import logging
 
 class QueueListener:
     def __init__(self, redis_url, bot : Bot, queue_key: str, db: int):
@@ -11,6 +12,8 @@ class QueueListener:
             self.texts = json.load(f)
         self.redis = aioredis.from_url(redis_url, db=db)
         self.user_data = {}
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
     
     def add_listening_user(self, user_id: int, message_id: int):
         self.user_data[user_id] = message_id
@@ -36,14 +39,13 @@ class QueueListener:
                 current_users = set()
                 async for user_id, message_id, index in self.__get_list_elements():
                     current_users.add(user_id)
+                    await self.bot.edit_message_text(chat_id=user_id, 
+                                                  message_id=message_id, 
+                                                  text=self.texts['messages']['queue_length'].format(queue_length=index))
                     if index == 0:
                         await self.bot.edit_message_text(chat_id=user_id, 
                                                         message_id=message_id, text=self.texts['messages']['queue_empty'])
                         self.user_data.pop(user_id)
-                    else:
-                        await self.bot.edit_message_text(chat_id=user_id, 
-                                                        message_id=message_id, 
-                                                        text=self.texts['messages']['queue_length'].format(queue_length=index))
                 for user_id in list(self.user_data.keys()):
                     if user_id not in current_users:
                         await self.bot.edit_message_text(chat_id=user_id, 
@@ -51,5 +53,6 @@ class QueueListener:
                                                         text=self.texts['messages']['queue_empty'])
                         self.user_data.pop(user_id)
                 await asyncio.sleep(1)
-            except Exception as _:
+            except Exception as e:
+                self.logger.error(f"Error: {e}")
                 await asyncio.sleep(1)
